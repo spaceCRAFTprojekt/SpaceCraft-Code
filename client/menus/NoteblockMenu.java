d9af7581f6a327a46d1ce842c0b6d060bdd7aafa
Max Hipp <34799688+maxxmodding@users.noreply.github.com>
2019-05-28 10:00:18 +0200

    v0.3.13_MH

--- client/menus/NoteblockMenu.java
+++ client/menus/NoteblockMenu.java
@@ -16,11 +16,9 @@ public class NoteblockMenu extends PlayerMenu{
         this.pos=pos;
         new MenuLabel(this, "Notes:", new VektorI(10,10), new VektorI(100,30));
         mta = new MenuTextArea(this,text,new VektorI(10,40),new VektorI(260,210));
-        boolean onPlanet=p.getPlayerC().isOnPlanet();
-        int sandboxIndex=p.getPlayerC().getSandboxIndex();
         mb = new MenuButton(this, "Save", new VektorI(170,260), new VektorI(100, 30)){
             public void onClick(){
-                Object[] menuParams={onPlanet,sandboxIndex,pos,mta.getText()};
+                Object[] menuParams={getPlayer().getCurrentMassIndex(),pos,mta.getText()};
                 Boolean success=(Boolean) (new Request(getPlayer().getID(),getPlayer().getRequestOut(),getPlayer().getRequestIn(),"Main.returnFromMenu",Boolean.class,"NoteblockMenu",menuParams).ret);
                 closeMenu();
             }
